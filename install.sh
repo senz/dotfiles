dae4d57dd921f3e88330ec772551482d872b3396
Kosta <kosta-codes@proton.me>
2024-04-21 17:39:06 +0000

    feat: env detection moved into config template

--- install.sh
+++ install.sh
@@ -2,63 +2,12 @@
 
 CHEZMOI_USER="senz"
 
-install_homebrew() {
-  if [ ! -x "$(command -v brew)" ]; then
-    xcode-select --install
-
-    echo "Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
-}
-
-install_chezmoi() {
-  if [ ! -x "$(command -v chezmoi)" ]; then
-    echo "Installing chezmoi"
-
-    if [ "$(uname)" == "Darwin" ]; then
-      brew install chezmoi
-    elif [ "$(uname)" == "Linux" ]; then
-      sh -c "$(curl -fsLS get.chezmoi.io)"
-    fi
-  fi
-}
-
-prepare_devcontainer() {
-  echo "Running in devcontainer/codespace"
-  if [ ! -f ~/.config/chezmoi/chezmoi.yaml ]; then
-    mkdir -p "$HOME/.config/chezmoi"
-    echo "pre-populating config from gitconfig"
-    cat <<EOF > "$HOME/.config/chezmoi/chezmoi.yaml"
----
-sourceDir: $HOME/dotfiles
-data:
-  fullname: $(git config --get user.name)
-  email: $(git config --get user.email)
-  recipient: $(git config --get user.signingkey)
-EOF
-  fi
-}
-
-run_chezmoi() {
-  if [ -n "$REMOTE_CONTAINERS" ]; then
-    # we have all the files in the dotfiles directory already
-    # so we can just run chezmoi init with the dotfiles directory and then purge
-    chezmoi init --debug --no-tty -S ~/dotfiles --apply --verbose
-  else
-    chezmoi init --debug --apply --verbose $CHEZMOI_USER
-  fi
-}
-
-# os specific pre-requisites
-if [ "$(uname)" == "Darwin" ]; then
-  install_homebrew
-fi
-
-install_chezmoi
-
-# in devcontainer/codespace
 if [ -n "$REMOTE_CONTAINERS" ]; then
-  prepare_devcontainer
+  echo "Running in remote container"
+  # in devcontainer we assume that the dotfiles are pre-downloaded and located in ~/dotfiles
+  # we pass the --no-tty flag because install invocation is non-interactive, so we handle this case in template by setting the default value
+  # sourceDir will be set to ~/dotfiles
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --debug --no-tty -S ~/dotfiles --apply
+else
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $CHEZMOI_USER
 fi
-
-run_chezmoi
