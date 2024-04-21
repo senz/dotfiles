5f469a68ee8b6d12794a8b3e53b11d3767dcc382
Kosta <kosta-codes@proton.me>
2024-04-21 15:19:37 +0000

    feat: installing brew and cz

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# if darwin and brew is not installed
+if [ "$(uname)" == "Darwin" ] && [ ! -x "$(command -v brew)" ]; then
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# if chezmoi is installed
+if [ -x "$(command -v chezmoi)" ]; then
+  echo "Running chezmoi init --apply --verbose"
+  chezmoi init --apply --verbose
+fi
+
+# in devcontainer/codespace
+# REMOTE_CONTAINERS is set
+if [ -n "$REMOTE_CONTAINERS" ]; then
+  echo "Running in devcontainer/codespace"
+fi
